26cdbfb98a2091e5ab543f9b67411e90ca99a8f5
Nischith Shetty <drnjshetty@gmail.com>
2023-01-02 22:31:43 +0530

    TRUNK-6158-> Making User lockout time configurable: commit 1 (#4226)

--- api/src/main/java/org/openmrs/api/db/hibernate/HibernateContextDAO.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/HibernateContextDAO.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.CacheMode;
